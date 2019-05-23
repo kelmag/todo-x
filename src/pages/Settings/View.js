1188dfd2f0e962859470c1b5a2fce3788b1a261e
kelmag <khaliilelmaghraoui@gmail.com>
2019-05-23 00:34:43 +0000

    define main pages content

--- src/pages/Settings/View.js
+++ src/pages/Settings/View.js
@@ -3,7 +3,32 @@ import React from 'react'
 export default function View() {
     return (
         <div>
-            settings
-    </div>
+            <div className="header">
+                <div className="top-bar">
+                    <span className="right-label"></span>
+                    <span className="center-label"></span>
+                    <span className="left-label"></span>
+                </div>
+                <div className="title">Settings</div>
+            </div>
+            <div class="config-container">
+                <div className="config-title">
+                    <span role="img" aria-label="config">⚙️</span>
+                    <span className="" >Settings</span>
+                </div>
+                <div class="config-line">
+                    Dark Mode
+                    <span role="img" aria-label="config">👍🏻</span>
+                </div>
+                <div class="config-line">
+                    Archive
+                    <span role="img" aria-label="config">👍🏻</span>
+                </div>
+                <div class="config-line">
+                    Done Tasks Timeout
+                    <span role="img" aria-label="config">👍🏻</span>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/pages/Todo/View.js
+++ src/pages/Todo/View.js
@@ -4,7 +4,31 @@ import React from 'react'
 export default function View() {
     return (
         <div>
-            todo
+            <div className="header">
+                <div className="top-bar">
+                    <span className="right-label"></span>
+                    <span className="center-label"></span>
+                    <span className="left-label"></span>
+                </div>
+                <div className="title">To Do List</div>
+            </div>
+            <div className="menu-bar">
+                <ul className="tabs">
+                    <li className="tab" id="todo">To Do</li>
+                    <li className="tab" id="done">Done</li>
+                </ul>
+                <button className="config-button" id="config-button"><span role="img" aria-label="config">⚙️</span></button>
+            </div>
+            <div className="tasks-todo">
+                <div className="task-line">task one</div>
+                <div className="task-line">task two</div>
+                <div className="task-line">task three</div>
+            </div>
+            <div className="tasks-done">
+                <div className="task-line">task one</div>
+                <div className="task-line">task two</div>
+                <div className="task-line">task three</div>
+            </div>
         </div>
     )
 }
