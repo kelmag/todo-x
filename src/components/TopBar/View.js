c79151e18a46e19c07f8d8154c90cc5791ddf339
kelmag <khaliilelmaghraoui@gmail.com>
2019-05-29 22:24:04 +0000

    added top-bar styling

--- src/components/TopBar/View.js
+++ src/components/TopBar/View.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './index.css';
 
 export default function View({ labels }) {
     return (
-        <div>
-            {(labels && labels[0]) ? <span className="right-label">{labels[0]}</span> : null}
-            {(labels && labels[1]) ? <span className="center-label">{labels[1]}</span> : null}
-            {(labels && labels[2]) ? <span className="f-label">{labels[2]}</span> : null}
+        <div className="top-bar">
+            {(labels && labels[0]) ? <span className="label right-label">{labels[0]}</span> : null}
+            {(labels && labels[1]) ? <span className="label center-label">{labels[1]}</span> : null}
+            {(labels && labels[2]) ? <span className="label left-label">{labels[2]}</span> : null}
         </div>
     )
 }
