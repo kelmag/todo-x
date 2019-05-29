b45c17fe465bca6d0d039b47ea70066f70adc5dd
kelmag <khaliilelmaghraoui@gmail.com>
2019-05-29 22:03:09 +0000

    added reach router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
 import Todo from './pages/Todo';
+import Settings from './pages/Settings';
+import Container from './components/Container';
+import { Router } from "@reach/router"
+
 
 function App() {
   return (
-    <Todo ></Todo>
+    <Container>
+      <Router>
+        <Todo path="/" />
+        <Settings path="/settings" />
+      </Router>
+    </Container>
   );
 }
 
